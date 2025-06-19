f9280d2e6765da5365498724fa755e586a62a37d
jbr284 <jbrosa2009@gmail.com>
2025-06-19 18:56:59 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+
+const CACHE_NAME = 'financeiro-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css', // Se houver
+  '/js/script.js',  // Se houver
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png',
+  // Adicione aqui outros arquivos que você deseja cachear
+];
+
+// Instalando o service worker e cacheando os arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativando o service worker e limpando caches antigos
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptando requisições e fornecendo arquivos do cache, se possível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
