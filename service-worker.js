bb1138dec8c5f46894f0d06923694b7dcf63fe3d
jbr284 <jbrosa2009@gmail.com>
2025-07-14 02:48:30 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,12 @@
-
-const CACHE_NAME = 'financeiro-cache-v1';
+const CACHE_NAME = 'controle-financeiro-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/css/style.css', // Se houver
-  '/js/script.js',  // Se houver
+  '/manifest.json',
   '/icon-192x192.png',
   '/icon-512x512.png',
-  // Adicione aqui outros arquivos que você deseja cachear
 ];
 
-// Instalando o service worker e cacheando os arquivos
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -19,27 +15,10 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Ativando o service worker e limpando caches antigos
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Interceptando requisições e fornecendo arquivos do cache, se possível
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
